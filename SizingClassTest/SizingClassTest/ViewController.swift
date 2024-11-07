df6f581034a2516233c0c181e201b7d20aa950eb
bmjohns <bretjohnsen@gmail.com>
2024-11-07 10:36:35 -0600

    Add files via upload

---
+++ SizingClassTest/SizingClassTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SizingClassTest
+//
+//  Created by Brett Johnsen on 10/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
