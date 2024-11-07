f4a33976caf50abf3fe2a2aa9990ef2c82b5197c
bmjohns <bretjohnsen@gmail.com>
2024-11-07 10:38:41 -0600

    Add files via upload

---
+++ SizingClassTestTests/SizingClassTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  SizingClassTestTests.swift
+//  SizingClassTestTests
+//
+//  Created by Brett Johnsen on 10/31/24.
+//
+
+import Testing
+@testable import SizingClassTest
+
+struct SizingClassTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
